38074bfaf9b4f7953ef51f1a56ca636abe732b02
francesco-divita-DEVELOPER <107550282+francesco-divita-DEVELOPER@users.noreply.github.com>
2022-07-20 15:53:27 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+/*$("#news").hide();
+$("#category").hide();
+$("#cards").hide();
+$("#focus").hide();
+$("body").mouseenter(function(){
+    $("#news").show(3000);
+    $("#category").show();
+    $("#cards").show();
+    $("#focus").show();
+
+
+})
+*/
